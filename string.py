13080e45f512e00fce9ab6cbefeeb421f9c61023
Alok Patel <95286756+alokmotion@users.noreply.github.com>
2022-03-02 12:19:20 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,73 @@
+'''
+TOPIC: String
+
+String - A data type that is used to store text 
+'''
+
+
+# name = 'alok'
+# name = "motion"
+
+# print(name)
+
+# print(type(name))
+
+
+'''
+Append and repeat
++   Can be used for append.
+*   Can be used for repeat.
+'''
+# A='patel'
+# print(name+A)
+# print(name+' '+A)
+# print(name *3)
+
+
+
+
+'''
+len() built-in python function.
+It can be used to get a number of characters in string
+'''
+# print(len(A))
+
+# print(len(name))
+
+
+
+
+'''
+How to use comma inside a string
+We can do that by using backslash escape code. 
+# '''
+# sentence = "I'love alok motion classes"
+# # OR
+# sentence2 = 'I\'love alok motion classes'
+
+
+# print(sentence)
+# print(sentence2)
+
+
+
+'''
+TODO Practice 
+
+Write code to get name, age, and height from user.
+Display the information in sentence like
+
+name is age year old and his height is height.
+
+NOTE To get user input from console we use built-in function
+called input("Sentence to display on console: ")
+'''
+# CODE START
+
+name=input("Enter your name")
+age=input("enter your age")
+height= input("enter your height")
+
+print(name+" si "+age+" year old, his/her height is "+height)
+
+# CODE END
