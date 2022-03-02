413e5f3cb70151b635a86da78fd752aa56448444
Alok Patel <95286756+alokmotion@users.noreply.github.com>
2022-03-02 12:13:21 +0530

    Add files via upload

---
+++ airthmatic operator.py
@@ -0,0 +1,113 @@
+'''
+TOPIC: Arithmetic Operators 
+
+For Addition : +
+For Subtraction : -
+For Multiplication : *
+For Division: /
+
+TODO For Example
+
+There are 5 Students in the class. All students have given their tests.
+Marks of the students are as follow:
+70, 80, 75, 65,and 50.
+
+What is the average result of the class.
+
+NOTE print() is a useful built-in function that displays input value
+     as text in the output.
+
+'''
+
+# CODE START
+
+# print(2+3)
+# print(9-4)
+# print(7*0)
+# print(7/6)
+
+# print((70+80++75+65+50)/5)
+
+# CODE END
+
+
+
+# Some Other Arithmetic opertors
+# Exponentiation : Used for power e.g. 3**2
+
+# CODE START
+
+# print (3**9)
+
+# print(2**1)
+# print(2**2)
+# print(2**3)
+# print(2**4)
+# print(2**5)
+# print(2**6)
+# print(2**7)
+# print(2**8)
+# print(2**9)
+# print(2**10)
+# print(2**11)
+# print(2**12)
+# print(2**13)
+# print(2**14)
+# print(2**15)
+# print(2**16)
+# print(2**17)
+# print(2**17)
+# print(2**18)
+# print(2**19)
+# print(2**20)
+# print(2**21)
+# print(2**22)
+# print(2**23)
+# print(2**24)
+# print(2**25)
+# print(3**2)
+
+
+# CODE END
+
+
+ 
+'''
+There's another operator that is sometimes mistaken for the exponential operator.
+The caret (^), this actually performs a more abscure operation called bitwise XOR
+NOTE Will Cover later.
+'''
+
+
+
+'''
+Another useful operator is this '%' sign which performs the modulo operation.
+It returns the remainder after you've divided the first number by the second.
+
+FOR EXAMPLE
+'''
+
+# CODE START
+# print(4%2)
+# 7-6 = 1
+# print(7%6)
+# 12-2 = 10-2 = 8-2 = 6-2 = 4-2 = 2-2 =0
+# print(12%2)
+
+# CODE END
+
+
+
+'''
+Integer division denoted by '//'. It divides one integer by another but
+rather than giving the exact answer, it rounds down the answer to an
+integer .i.e. without decimal point
+'''
+
+# CODE START
+
+# print(3//4)
+# print(9//7)
+
+# CODE END
+
